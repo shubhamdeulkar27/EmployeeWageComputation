075fe945759ada6989ec468363180b96f87af06c
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-31 17:30:53 +0530

    Refactored: Using Switch Case To Decide The Employee Working Hours

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        
+        //CONSTANTS
+        private const int IS_PART_TIME = 1;
+        private const int IS_FULL_TIME = 2;
+        private const int WAGE_PER_HOUR = 20;
+
         //FUNCTION TO CHECK EMPLOYEE ATTENDENCE RANDOMLY
         public int checkEmployee()
         {
@@ -12,32 +16,34 @@ namespace EmployeeWageComputation
             int randomCheck = random.Next(3);
             return randomCheck;
         }
+        public int checkEmployeeHour(int employeeAttendenceCheck) 
+        {
+            int employeeHours = 0;
+            switch (employeeAttendenceCheck)
+            {
+                case IS_FULL_TIME:
+                    employeeHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    employeeHours = 4;
+                    break;
+                default:
+                    employeeHours = 0;
+                    break;
+            }
+            return employeeHours;
+        }
         static void Main(string[] args)
-        {   //CONSTANTS
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int WAGE_PER_HOUR = 20;
-
+        {   
             //VARIABLE
             int employeeHours = 0;
             int dailyWage = 0;
 
             Program program = new Program();
             int employeeAttendenceCheck = program.checkEmployee();
+            employeeHours=program.checkEmployeeHour(employeeAttendenceCheck);
 
-            if (employeeAttendenceCheck == IS_FULL_TIME)
-            {
-                employeeHours = 8;
-            }
-            else if (employeeAttendenceCheck == IS_PART_TIME)
-            {
-                employeeHours = 4;
-            }
-            else
-            {
-                employeeHours = 0;
-            }
-
+            //CALCULATING DAILY WAGE
             dailyWage = WAGE_PER_HOUR * employeeHours;
             Console.WriteLine($"Daily Employee Wage is {dailyWage}");
         }
