369f54daa79a99a0d731ddc6f92d9e0284ff5fb0
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-31 14:50:43 +0530

    Refactored: Calculating Part Time Employee Wage

--- Program.cs
+++ Program.cs
@@ -9,25 +9,29 @@ namespace EmployeeWageComputation
         public int checkEmployee()
         {
             Random random = new Random();
-            int randomCheck = random.Next(2);
+            int randomCheck = random.Next(3);
             return randomCheck;
         }
         static void Main(string[] args)
         {   //CONSTANTS
-            int IS_PRESENT = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int WAGE_PER_HOUR = 20;
 
-            //VARIABLES
-            int employeeAttendence = 0;
+            //VARIABLE
             int employeeHours = 0;
             int dailyWage = 0;
 
             Program program = new Program();
             int employeeAttendenceCheck = program.checkEmployee();
 
-            if (employeeAttendenceCheck == IS_PRESENT)
+            if (employeeAttendenceCheck == IS_FULL_TIME)
             {
                 employeeHours = 8;
+            }
+            else if (employeeAttendenceCheck == IS_PART_TIME)
+            {
+                employeeHours = 4;
             }
             else
             {
