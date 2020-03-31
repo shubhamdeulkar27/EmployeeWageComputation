8c0a32801a42f768a726b8e2ab8c71e2cf118bc2
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-31 14:26:14 +0530

    Refactored: Checking Employee Present Or Not

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        
+        public int checkEmployee()
+        {
+            Random random = new Random();
+            int randomCheck = random.Next(2);
+            return randomCheck;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            int IS_PRESENT = 1;
+            int employeeAttendence = 0;
+            Program program = new Program();
+            int employeeAttendenceCheck = program.checkEmployee();
+            if (employeeAttendenceCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Not Present");
+            }
         }
     }
 }
