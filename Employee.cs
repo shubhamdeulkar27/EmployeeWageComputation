b917e1071499583399d92fabf658ee0174c674c1
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-05 14:24:10 +0530

    Refactored: Commenting Done For Employee Class

--- Employee.cs
+++ Employee.cs
@@ -4,7 +4,8 @@
 /// Employee Class For Setting and Getting Data From Employee Object
 /// </summary>
 public class Employee
-{
+{   
+    //Variables
     private int dailyWage = 0;
     private int monthlyWage = 0;
     private int workingHours = 0;
@@ -12,6 +13,10 @@ public class Employee
     private int employeeHours = 0;
     private int employeeAttendenceCheck = 0;
 
+
+    /// <summary>
+    /// Getters and Setters for data encapsulation.
+    /// </summary>
     public int DailyWage { get => dailyWage; set => dailyWage = value; }
     public int MonthlyWage { get => monthlyWage; set => monthlyWage = value; }
     public int WorkingHours { get => workingHours; set => workingHours = value; }
