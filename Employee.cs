78165a50f96d9f20a2c66e15e005ae436494e624
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-05 08:26:39 +0530

    Refactored: Refactored Using OOPs Concept

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Employee Class For Setting and Getting Data From Employee Object
+/// </summary>
+public class Employee
+{
+    private int dailyWage = 0;
+    private int monthlyWage = 0;
+    private int workingHours = 0;
+    private int workingDays = 0;
+    private int employeeHours = 0;
+    private int employeeAttendenceCheck = 0;
+
+    public int DailyWage { get => dailyWage; set => dailyWage = value; }
+    public int MonthlyWage { get => monthlyWage; set => monthlyWage = value; }
+    public int WorkingHours { get => workingHours; set => workingHours = value; }
+    public int EmployeeHours { get => employeeHours; set => employeeHours = value; }
+    public int EmployeeAttendenceCheck { get => employeeAttendenceCheck; set => employeeAttendenceCheck = value; }
+    public int WorkingDays { get => workingDays; set => workingDays = value; }
+   
+}
--- Program.cs
+++ Program.cs
@@ -8,28 +8,39 @@ namespace EmployeeWageComputation
         //CONSTANTS
         private const int IS_PART_TIME = 1;
         private const int IS_FULL_TIME = 2;
+        private const int PART_TIME_HOURS = 4;
+        private const int FULL_TIME_HOURS = 8;
         private const int WAGE_PER_HOUR = 20;
         private const int MAX_WORKING_DAYS = 20;
         private const int MAX_WORKING_HOURS = 100;
 
-        //FUNCTION TO CHECK EMPLOYEE ATTENDENCE RANDOMLY
-        public int checkEmployee()
+        /// <summary>
+        /// FUNCTION TO CHECK EMPLOYEE ATTENDENCE RANDOMLY
+        /// USES RANDOM CLASS'S NEXT FUNCTION
+        /// </summary>
+        /// <returns></returns>
+        public int CheckEmployee()
         {
             Random random = new Random();
             int randomCheck = random.Next(3);
             return randomCheck;
         }
-        //FUNCTION TO CHECK EMPLOYEE WORKING HOURS
-        public int checkEmployeeHour(int employeeAttendenceCheck) 
+
+        /// <summary>
+        /// FUNCTION TO CHECK EMPLOYEE WORKING HOURS
+        /// </summary>
+        /// <param name="employeeAttendenceCheck"></param>
+        /// <returns></returns>
+        public int CheckEmployeeHour(int employeeAttendenceCheck) 
         {
             int employeeHours = 0;
             switch (employeeAttendenceCheck)
             {
                 case IS_FULL_TIME:
-                    employeeHours = 8;
+                    employeeHours = FULL_TIME_HOURS;
                     break;
                 case IS_PART_TIME:
-                    employeeHours = 4;
+                    employeeHours = PART_TIME_HOURS;
                     break;
                 default:
                     employeeHours = 0;
@@ -38,45 +49,44 @@ namespace EmployeeWageComputation
             return employeeHours;
         }
 
-        //FUNCTION FOR CALCULATING MONTHLY WAGES
-        public Dictionary <int,int> calculateWageForMonth(int MAX_WORKING_DAYS) 
+        /// <summary>
+        /// FUNCTION FOR CALCULATING MONTHLY WAGES
+        /// </summary>
+        /// <param name="MAX_WORKING_DAYS"></param>
+        /// <returns></returns>
+        public Dictionary <string,int> CalculateWageForMonth(int MAX_WORKING_DAYS) 
         {
-            //VARIABLE
-            int dailyWage = 0;
-            int monthlyWage = 0;
-            int workingHours = 0;
-            int workingDays = 0;
-            int employeeHours = 0;
-            
-            Dictionary<int,int> results = new Dictionary<int, int>();
-
-            while (workingDays<MAX_WORKING_DAYS && workingHours<MAX_WORKING_HOURS)
+            Employee employee = new Employee();
+            Dictionary<string,int> results = new Dictionary<string, int>();
+            while (employee.WorkingDays<MAX_WORKING_DAYS && employee.WorkingHours<MAX_WORKING_HOURS)
             {
-                
+                employee.WorkingDays++;
                 Program program = new Program();
-                int employeeAttendenceCheck = program.checkEmployee();
-                employeeHours = program.checkEmployeeHour(employeeAttendenceCheck);
-                workingHours = workingHours + employeeHours;
+                employee.EmployeeAttendenceCheck = program.CheckEmployee();
+                employee.EmployeeHours = program.CheckEmployeeHour(employee.EmployeeAttendenceCheck);
+                employee.WorkingHours = employee.WorkingHours + employee.EmployeeHours;
  
-                //CALCULATING DAILY WAGE
-                dailyWage = WAGE_PER_HOUR * employeeHours;
-                results[workingDays] = dailyWage;
-                monthlyWage = monthlyWage + dailyWage;
-                workingDays++;
+                //CALCULATING DAILY WAGE AND MONTHLY WAGE
+                employee.DailyWage = WAGE_PER_HOUR * employee.EmployeeHours;
+                results[$"Day {employee.WorkingDays}"] = employee.DailyWage;
+                employee.MonthlyWage = employee.MonthlyWage + employee.DailyWage;
             }
-            results[workingDays] = monthlyWage;
-            Console.WriteLine(workingHours);
+            results["Monthly Wage"] = employee.MonthlyWage;
             return results;
         }
 
+        /// <summary>
+        /// MAIN FUNCTION CALCULATE WAGES FOR MONTH AND STORE IT TO DICTIONARY
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Program program = new Program();
-            Dictionary <int,int> Wages = program.calculateWageForMonth(MAX_WORKING_DAYS);
+            Dictionary <string,int> Wages = program.CalculateWageForMonth(MAX_WORKING_DAYS);
             Console.WriteLine("Monthly Employee Wage is : ");
-            foreach (KeyValuePair<int, int> kv in Wages)
+            foreach (KeyValuePair<string, int> kv in Wages)
             {
-                Console.WriteLine(kv.Key+" "+kv.Value);
+                Console.WriteLine(kv.Key+"-"+kv.Value);
             }
         }
     }
